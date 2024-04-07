71474969fa9abb0c42c824e832576c1de42c290c
segovelo <gauchosinpingo@gmail.com>
2024-04-07 15:34:58 +0100

    Changes to add routing and pass id as uri param.

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
+}
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { HouseInterface } from '../house-interface'; 
+import {RouterModule}  from '@angular/router'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
